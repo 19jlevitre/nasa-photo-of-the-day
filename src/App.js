02143e8f08929c03834870e5f3bbd86cbcb18737
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-09-08 15:53:36 -0600

    done and done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BASE_URL, API_KEY} from './Constants';
+import axios from 'axios';
+
 
 function App() {
+  const [picOfTheDay, setPicOfTheDay] = useState({});
+  
+  useEffect(() => {
+   axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      console.log(res.data);
+      setPicOfTheDay(res.data);
+      // console.log(picOfTheDay);
+    }).catch(err => console.error(err));
+  }, [])
+    
+  const Pic = props => (
+   <div className="picture">
+    <h2>Your NASA photo of the day!: {props.pic.title} </h2>
+    <img 
+    className='image-of-the-day'
+    alt='NASA img of the day'
+    src={props.pic.url}/> 
+    
+    </div>
+  )
+  
+  
+  
+  
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header><h1> THE APOD SITE </h1></header>
+      <div className="Content">
+       {picOfTheDay.length === 0 && <p>LOADING</p>}
+       <Pic pic={picOfTheDay}/>
+       
+       
+       
+
+      </div>
     </div>
   );
 }
---
+++ src/Constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'pwVUTtX1cIFyjuD4ryNesxaN1C9j0jItpdfXdiWz'
