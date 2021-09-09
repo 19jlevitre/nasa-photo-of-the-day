3f18d14858876c455c196b91c7ead9ada5c2cbc7
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-09-09 14:03:16 -0600

    done for real

--- src/App.js
+++ src/App.js
@@ -2,7 +2,53 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BASE_URL, API_KEY} from './Constants';
 import axios from 'axios';
+import styled from "styled-components";
 
+const StyledHeader = styled.header`
+h1{
+  color: grey;
+}
+nav{
+  display: flex;
+  justify-content: space-evenly;
+  
+  a{
+    text-decoration: none;
+    color: blue;
+    font-family: sans-serrif;
+  }
+
+}
+`
+
+const StyledImg = styled.div`
+
+border: solid 2px purple;
+border-radius: 10px;
+margin: 5% 30%;
+background-color: black;
+
+img {
+  height: 40vh;
+  margin-bottom: 10%;
+}
+h2 {
+  color: white;
+}
+`
+const StyledFooter = styled.footer`
+nav{
+  display: flex;
+  justify-content: space-evenly;
+  
+  a{
+    text-decoration: none;
+    color: blue;
+    font-family: sans-serrif;
+  }
+
+}
+`
 
 function App() {
   const [picOfTheDay, setPicOfTheDay] = useState({});
@@ -17,14 +63,14 @@ function App() {
   }, [])
     
   const Pic = props => (
-   <div className="picture">
-    <h2>Your NASA photo of the day!: {props.pic.title} </h2>
+   <StyledImg className="picture">
+    <h2>Your NASA photo of the day!:  "{props.pic.title}" </h2>
     <img 
     className='image-of-the-day'
     alt='NASA img of the day'
     src={props.pic.url}/> 
     
-    </div>
+    </StyledImg>
   )
   
   
@@ -33,15 +79,32 @@ function App() {
   
   return (
     <div className="App">
-      <header><h1> THE APOD SITE </h1></header>
+      <StyledHeader><h1> THE APOD SITE </h1>
+      <nav>
+        <a href= ''>Space</a>
+        <a href= ''>The Final</a>
+        <a href= ''>Frontier</a>
+        <a href= ''>Red Shirt</a>
+      </nav>
+      </StyledHeader>
       <div className="Content">
        {picOfTheDay.length === 0 && <p>LOADING</p>}
+       <Pic pic={picOfTheDay}/>
        <Pic pic={picOfTheDay}/>
        
        
        
 
       </div>
+      <StyledFooter>
+        <h2>The End</h2>
+        <nav>
+        <a href= ''>Space</a>
+        <a href= ''>The Final</a>
+        <a href= ''>Frontier</a>
+        <a href= ''>Red Shirt</a>
+      </nav>
+      </StyledFooter>
     </div>
   );
 }
